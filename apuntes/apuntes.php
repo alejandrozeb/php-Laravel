537366aed149b04862cd57d1a84462a5b7f554a7
unknown <alejandrojzeballos@gmail.com>
2021-02-08 00:47:08 -0400

    sintaxis y comentarios en php

--- apuntes/apuntes.php
+++ apuntes/apuntes.php
@@ -6,5 +6,8 @@
     https://getcomposer.org/download/
     descargamos el instalador y lo ejecutamos, al redireecion la version debe apuntar a un php mayo 7.3 por algunas caracteriticas propias que usa composer.
 
+    
 
  -->
+
+ <?php  /* esta es la etiqueta de inicia de cualquier archivo php */
---
+++ fundamentos/1-introduccion.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Answer{
+    //comentario una linea
+    #comentario una linea opcion 2
+    /* comentario varias lineas */
+    /* toda sentencia e php necesita finalizar con un ;, esto denota el final de una sentencia */
+
+    /* formato de comentario con jerarquia */
+
+    /* 
+    * Comentarios de varias
+    * lineas para
+    * palabra
+    */
+
+    protected $clients = [];
+    protected $insurers = [];
+}
