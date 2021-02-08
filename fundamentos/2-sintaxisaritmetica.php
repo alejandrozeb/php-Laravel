49b43d7172f9e72a90768bf58bd8f22b59044ab6
unknown <alejandrojzeballos@gmail.com>
2021-02-08 00:59:59 -0400

    {Add} operadores en php

---
+++ fundamentos/2-sintaxisaritmetica.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Asignacion
+$num = 9;   //asignamos una variable
+$lang = [
+    'es' => 'español',
+    'en' => 'ingles',
+];
+
+// Aritmetica
+echo "suma 2+2". ((int)2+(int)2);
+echo "resta 2-2". ((int)2-(int)2);
+echo "multiplica 2-2". (2*2);
+echo "div 2-2". (2/2);
+echo "Modulo 2-2". (2%2);   //nos devulelve el residuo de la operacion
+echo "exponencial 2-2". (2**2); //exponencial
+
+/* notas
+No es necesario usar (int) en todas la operaciones de suma o resta en php, php reconoce la operacion  y el tipo de dato
+- antes de declarar cualquier variable usamos $
+- => asigna el valor en estrctura como arreglos
+*/
