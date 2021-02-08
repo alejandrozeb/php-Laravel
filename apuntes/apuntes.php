9724963f5916b80f142a55f9856ed95d5f27f322
unknown <alejandrojzeballos@gmail.com>
2021-02-08 00:36:01 -0400

    {Add} instalando composer

---
+++ apuntes/apuntes.php
@@ -0,0 +1,10 @@
+<!-- 
+    debemos tener instalado
+    VStudio code o algun ide
+    wamp o xamp
+    composer
+    https://getcomposer.org/download/
+    descargamos el instalador y lo ejecutamos, al redireecion la version debe apuntar a un php mayo 7.3 por algunas caracteriticas propias que usa composer.
+
+
+ -->
