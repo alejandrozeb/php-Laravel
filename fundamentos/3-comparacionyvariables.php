f64e6f82067563f553153abf21b80818d075d618
unknown <alejandrojzeballos@gmail.com>
2021-02-09 00:20:47 -0400

    {add} comparacion y variables

---
+++ fundamentos/3-comparacionyvariables.php
@@ -0,0 +1,14 @@
+<?php
+//comparracion
+//igual ==, valor '9'==9
+//igual ===, valor exacto - tipo 9 === 9
+//diferencias != valor
+//diferencia !==, valor -tipo
+//<.>, <=,>=
+
+//Variables variables
+$app = 'name';//name
+$name='platzi';
+
+echo $app; //name
+echo $$app; //platzi
