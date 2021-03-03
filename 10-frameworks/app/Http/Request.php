a51e2e26f21d74d9bd2cd9678763813e609e9113
unknown <alejandrojzeballos@gmail.com>
2021-03-02 23:56:57 -0400

    {add} Request configuracion de aceptar peticiones

--- 10-frameworks/app/Http/Request.php
+++ 10-frameworks/app/Http/Request.php
@@ -13,11 +13,40 @@ namespace App\Http; //referencia a la carpeta
          $this->setController();
          $this->setMethod();
     }
-
+    //llena los datosa proiedades
     public function setController(){
         $this->controller = empty($this->segments[1])
             ? 'home'
             : $this->segments[2];
+    }
+
+    public function setMethod(){
+        $this->method = empty($this->segments[2])
+            ?'index'
+            : $this->segments[2];
+    }
+    // obtiene la informacion configurada
+    public function getController(){
+        //home , HomeController para que este en estandar
+        $controller = ucfirst($this->controller);
+
+        return "App\Http\Controllers\\{$controller}Controller";
+    }
+
+    public function getMethod(){
+        return $this->method;
+    }
+    //metodo que ejecuta todo
+    public function send(){
+        $controller = $this->getController();
+        $method =  $this->getMethod();
+
+        $response = call_user_func([
+            new $controller,
+            $method
+        ]);
+
+        $response->send();
     }
 
  }
